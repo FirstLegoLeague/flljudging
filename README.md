148365271f1459bb940213b6f255970d418321f1
Kenny Meesters <k.meesters@gmail.com>
2014-12-10 15:05:06 +0100

    Scaffolding of application, screens, data
    Initial Scaffolding of pages and Angular enviroment. First design for
    rubric form. Data structure for teams, rubric questions and judging
    categories.

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
-flljudging
+FLL-Judging
 ==========
 
-FLL Judging application
+FLL Judging application is a first step towards a complete digital judging system. Please note that this is still very much under development and should not be used in live enviroments. 
+
+Goal
+----
+The goal of the application is to support the judging process according the international FLL guidelines. This includes (but is not limited to) the following:
+* Digital rubrics, with comments
+* Ranking & nominations support
+* Dissementation of rubrics (feedback) to teams
+* Import and export of data (to and from judging lite or other applications)
+
+Development
+-----------
+The application is built as an webapplication using angular. Like the scoring application the ambitioin is to support a wide range of configurations (tournament- and network use). It should also pull and push data to and from other applications. 
+
+However the first step is to move all the judging forms (rubrics and nominations) to a digital version. 
+
+Prerequisites & Installation
+============================
+The project is based on angularJS and setup using Yeoman generator. It requires
+
+Install
+-------
+To install the required packages run `npm install` or use `bower install` to install the individual packages. The following packages are included:
+
+* angular
+* angular-mocks
+* angular-scenario
+* angular-animate
+* angular-aria
+* angular-cookies
+* angular-messages
+* angular-resource
+* angluar-sanitize
+
+Run
+---
+To preview (run) the application open a command prompt and execute: `grunt serve`
+
+For testing execute `grunt test`
+
+To build the application run `grunt`
+
+Project
+=======
+If you have ideas, questions or want to contribute, please head over to the [issue tracker](https://github.com/FirstLegoLeague/flljudging/issues)
+
+Optionally, you can also contact [Kenny Meesters](mailto:k.meesters@gmail.com)
+
---
+++ app/scripts/app.js
@@ -0,0 +1,123 @@
+'use strict';
+// script.js
+
+    // create the module and name it scotchApp
+        // also include ngRoute for all our routing needs
+    var flljudgingApp = angular.module('flljudgingApp', ['ngRoute']);
+	
+	
+    // configure our routes
+    flljudgingApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+
+            // route for the rubrics pages
+            .when('/RubricsRD', {
+                templateUrl : 'pages/rubrics.html',
+                controller  : 'rubricsRDController'
+            })
+			
+			.when('/RubricsPR', {
+                templateUrl : 'pages/rubrics.html',
+                controller  : 'rubricsPRController'
+            })
+
+			.when('/RubricsCV', {
+                templateUrl : 'pages/rubrics.html',
+                controller  : 'rubricsCVController'
+            })
+			
+            // route for the nominations page
+            .when('/nominations', {
+                templateUrl : 'pages/nominations.html',
+                controller  : 'nominationsController'
+            })
+			// route for the rankings page
+            .when('/rankings', {
+                templateUrl : 'pages/rankings.html',
+                controller  : 'rankingsController'
+            })
+			// route for the admin page
+            .when('/admin', {
+                templateUrl : 'pages/admin.html',
+                controller  : 'adminController'
+            });
+    });
+
+
+    // create the controller and inject Angular's $scope
+    flljudgingApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
+
+    flljudgingApp.controller('rubricsRDController', function($scope) {
+        $scope.message = 'The Robot Design rubric will go here.';
+		$scope.rubricCategory = 'Robot Design';
+		$scope.rubricIcon = 'RobotDesign.png';
+    });
+	
+	flljudgingApp.controller('rubricsPRController', function($scope) {
+        $scope.message = 'The Project rubric will go here.';
+		$scope.rubricCategory = 'Project';
+		$scope.rubricIcon = 'Project.png';
+    });
+	
+	flljudgingApp.controller('rubricsCVController', function($scope) {
+        $scope.message = 'The Core Values rubric will go here.';
+		$scope.rubricCategory = 'Core Values';
+		$scope.rubricIcon = 'CoreValues.png';
+    });
+
+    flljudgingApp.controller('nominationsController', function($scope) {
+        $scope.message = 'The nominations form will go here.';
+		
+    });
+    flljudgingApp.controller('rankingsController', function($scope) {
+        $scope.message = 'The rankings form will go here.';
+		
+    });
+    flljudgingApp.controller('adminController', function($scope) {
+        $scope.message = 'The admin form will go here.';
+		
+    });
+	
+	flljudgingApp.controller('Selections', function ($scope, $http) {
+    $scope.selectedTeam = null;
+    $scope.teams = [];
+	$scope.selectJudgingPanel = null;
+    $scope.judgingpanels = [];
+	
+
+		$http({	
+			method: 'GET',
+			url: '/data/teams.json',
+			data: { applicationId: 3 }
+			}).success(function (result) {
+			$scope.teams = result;
+		});
+		$http({
+			method: 'GET',
+			url: '/data/judgingpanels.json',
+			data: { applicationId: 3 }
+			}).success(function (result) {
+			$scope.judgingpanels = result;
+		});
+	});	
+	
+	flljudgingApp.controller('RubricForm', function ($scope, $http) {
+    $scope.rubricQuestions = [];
+	
+		$http({
+			method: 'GET',
+			url: '/data/questions.json',
+			data: { applicationId: 3 }
+			}).success(function (result) {
+			$scope.rubricQuestions = result;
+		});
+	});
