f703ac0ba43b772f04c113f8a0a5c530fb1d0746
Sools,Tommy T.P.A <t.sools@fontys.nl>
2016-07-17 04:55:19 +0200

    Team list filtering based on rubrics. Better JSON output file format. Proper handling of duplicate team numbers dragged into either direction.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -230,6 +230,48 @@ flljudgingApp.controller('NominationsForm', function ($scope, $http, $window) {
 	$scope.teams = [];
 	$scope.teamsAll = [];
 	$scope.teamsSorted = [];
+	/*
+		effect: determines the AngularJS list based on the HTML class name (teamlist/award0/award1/award2/overall);
+		pre: (list == ui.item.sortable.droptarget[0]) || (list == event.sender);
+	*/
+	$scope.getSortableList = function(list) {
+		var teamsSorted;
+		// convert the award numbers that can be larger than 2 to numbers in the range 0..2.
+		var awardsFiltered = $scope.awards.filter(function(award) {
+			return award.category == $scope.rubricCategory;
+		});
+		if ($(list).hasClass('teamlist')) {
+			teamsSorted = $scope.teams;
+		}
+		else if ($(list).hasClass('overall')) {
+			teamsSorted = $scope.teamsSorted;
+		}
+		else {
+			for (var i = 0; i < awardsFiltered.length; i++) {
+				if ($(list).hasClass('award' + i)) {
+					teamsSorted = awardsFiltered[i].teamsSorted;
+				}
+			}
+		}
+		return teamsSorted;
+	}
+	/*
+		effect: removes the last team number matching with teamNumber from the array list;
+	*/
+	function removeFromList(list, teamNumber) {
+		var found = -1;
+		var i = 0;
+		list.forEach(function(teamSorted) {
+			if (teamSorted.number == teamNumber) {
+				found = i;
+			}
+			i++;
+		});
+		if (found > -1) {
+			list = list.splice(found, 1);
+		}
+		return list;
+	}
 	$scope.sortableOptions = {
 		// see: https://github.com/angular-ui/ui-sortable/
 		// and http://angular-ui.github.io/ui-sortable/
@@ -240,20 +282,43 @@ flljudgingApp.controller('NominationsForm', function ($scope, $http, $window) {
 			} else {
 
 			} 
-			console.log($scope.teamsSorted);
+			if (ui.item.sortable.droptarget != null) {
+				var teamsSortedTarget = $scope.getSortableList(ui.item.sortable.droptarget[0]);
+				//console.log(ui.item.sortable.sourceModel);
+				//console.log(ui.item.sortable.droptargetModel);
+				if (teamsSortedTarget == $scope.teams) {
+					if (!ui.item.sortable.received) {
+						// if the teamList is the target list, always cancel the drag-and-drop action and remove the source element.
+						var teamsSortedSource = $scope.getSortableList(event.target);
+						teamsSortedSource = removeFromList(teamsSortedSource, ui.item.sortable.model.number);
+						ui.item.sortable.cancel();
+					}/*
+					if (ui.item.sortable.received) {
+						// if the team number already exists in the teamList, remove the last one from the source list.
+						teamsSortedTarget = removeFromList(teamsSortedTarget, ui.item.sortable.model.number);
+					}*/
+				}
+				else if (!ui.item.sortable.received && (teamsSortedTarget != null)) {
+					// if the team already exists in the target list, cancel the drag-and-drop action (but leave it in the source list).
+					if (teamsSortedTarget.some(function(teamSorted) {
+						return teamSorted.number == ui.item.sortable.model.number;
+					})) {
+						ui.item.sortable.cancel();
+					}
+				}
+			}
 		},
 		stop: function(event, ui) {
-			console.log(event.target);
-			//if ($(event.target).hasClass('teamlist')) {
-			if ($(ui.item.sortable.droptarget[0]).hasClass('teamlist')) {
-				// create spare copy of teamlist.
-				$scope.teamsAll = $scope.teams.slice();
+			if (ui.item.sortable.droptarget != null) {
+				if ($(ui.item.sortable.droptarget[0]).hasClass('teamlist')) {
+					// create spare copy of teamlist.
+					$scope.teamsAll = $scope.teams.slice();
+				}
+				else {
+					// clone teamlist from spare copy.
+					$scope.teams = $scope.teamsAll.slice();
+				}
 			}
-			else {
-				// clone teamlist from spare copy.
-				$scope.teams = $scope.teamsAll.slice();
-			}
-			
 		}
 	};
 	
@@ -287,7 +352,55 @@ flljudgingApp.controller('NominationsForm', function ($scope, $http, $window) {
 		var numTeams = Object.keys($scope.teams).length;
 		console.log(numTeams + " teams successfully fetched!");
 	});
-	
+	$scope.clearTeams = function() {
+		this.teams = [];
+		this.teamsAll = [];
+	}
+	$scope.clearRankings = function() {
+		this.teamsSorted = [];
+	}
+	$scope.clearAwards = function() {
+		this.awards.forEach(function(award) {
+			award.teamsSorted = [];
+		});
+	}
+	$scope.collectTeams = function(panel) {
+		/*
+		file specs:
+			completed_rubrics\rubric_<category>_<team>.json
+			.Category
+			.Panel.panel
+			.Team.number
+		*/
+		$http({
+			method: 'GET',
+			url: '/fs/completed_rubrics/'
+		}).success(function(data) {
+			var files = data.split("\n");
+			files.forEach(function(filename) {
+				$http({
+					method: 'GET',
+					url: '/fs/completed_rubrics/' + filename
+				}).success(function(data) {
+					if (data.Panel.panel == panel) {
+						if (!$scope.teams.some(function(team) {
+							return team.number == data.Team.number;
+						})) {
+							console.log(panel + " " + data.Team.number);
+							$scope.teams.push(data.Team);
+							$scope.teamsAll.push(data.Team);
+						}
+					}
+				}).error(function() {
+					console.log("Failed!");
+					console.log("--------");
+				});	
+			});
+		}).error(function() {
+			console.log("Failed!");
+			console.log("--------");
+		});	
+	}
 	$scope.isTeamRanked = function(team) {
 		var result = false;
 		this.teamsSorted.forEach(function(sortedTeam) {
@@ -296,49 +409,77 @@ flljudgingApp.controller('NominationsForm', function ($scope, $http, $window) {
 			}
 		});
 		return result;
-	};
+	}
 	$scope.areAllRanked = function() {
 		return this.teamsSorted.length >= this.teams.length;
-	};
+	}
 	$scope.isPanelSelected = function() { 
 		return this.selectJudgingPanel && this.selectJudgingPanel.panel;
 	}
+	$scope.ChangeForm = function() {
+		if (this.selectJudgingPanel) {
+			this.clearRankings();
+			this.clearAwards();
+			this.clearTeams();
+			this.collectTeams(this.selectJudgingPanel.panel);
+		}
+	}
 	$scope.TestForm = function(){
 		this.teamsSorted = this.teams.slice();
-		this.awards.forEach(function(award) {
-			award.teamsSorted = [];
-		});
+		//this.clearAwards();
 		console.log("Form Shuffled!");
 	}
 	$scope.ResetForm = function(){
-		this.teamsSorted = [];
-		this.awards.forEach(function(award) {
-			award.teamsSorted = [];
-		});
+		this.clearRankings();
+		this.clearAwards();
 		console.log("Form Reset!");
 	}
 	$scope.isSubmitEnabled = function() {
 		return this.isPanelSelected() && this.areAllRanked();
-	};
+	}
+	function getRanking(teamsSorted) {
+		var ranking = [];
+		var i = 0;
+		teamsSorted.forEach(function(teamSorted) {
+			i++;
+			ranking.push({
+				"rank": i,
+				"teamNumber": teamSorted.number,
+				"teamName": teamSorted.name
+			});
+		});
+		return ranking;
+	}
 	$scope.SubmitForm = function (){
-		var filename = "nomination_" + $scope.rubricCategory + "_" + $scope.selectJudgingPanel.panel + "_" + Date.now();
+		var filename = "nomination_" + this.selectJudgingPanel.panel.replace(" ", "") + "_" + Date.now();
+		var rankings = [];
+		rankings.push({
+			"ranking": "overall",
+			"ranks": getRanking(this.teamsSorted)
+		});
+		this.awards.forEach(function(award) {
+			if (award.category === $scope.rubricCategory) {
+				rankings.push({
+					"ranking": award.award,
+					"ranks": getRanking(award.teamsSorted)
+				});
+			}
+		});
 		$http({
 			method: 'POST',
 			url: '/fs/completed_nominations/' + filename + '.json',
 			headers: { 'Content-Type': 'application/json; charset=UTF-8' },
 			data: { 
-				panel: $scope.selectJudgingPanel.panel,
-				ranking: $scope.teamsSorted,
-				awards: $scope.awards.filter(function(award) {
-					return award.category === $scope.rubricCategory;
-				})
+				category: this.rubricCategory,
+				panel: this.selectJudgingPanel.panel,
+				rankings: rankings
 			}
-		}).success(function (){
+		}).success(function() {
 			console.log("Saved!");
 			console.log("--------");
 			$window.alert('Thank you for submitting the ' + $scope.selectJudgingPanel.panel + ' panel nominations.');
 			$scope.ResetForm();
-		}).error(function (){
+		}).error(function() {
 			console.log("Failed!");
 			console.log("--------");
 		});	
