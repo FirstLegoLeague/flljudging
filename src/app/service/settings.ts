c0428c06863b9073093d1baa6d34a3cecca49307
Ven <m.w.c.v.d.ven@student.tue.nl>
2018-02-09 16:39:42 +0100

    Revert "Switch back to new angular (#32)"
    This reverts commit faba72e1c16cdff3f4e0e8165b9b32e27ce99032.

--- src/app/service/settings.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Globals } from '../base/globals';
-import { HttpClient } from '@angular/common/http';
-
-export class SettingsService {
-}
